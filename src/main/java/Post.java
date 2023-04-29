a27b4605109a8c13f7dc65aab8ac477fcec84d3c
Natalia <natysun07@rambler.ru>
2023-04-29 14:12:11 +0300

    Information re-ordering

--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -1,11 +1,11 @@
 public class Post {
-    public  String name;
+    public FormDate birthday;
+    public String name;
     public String passport;
     public String patronymic;
     public String phone;
-    public  String surname;
+    public String surname;
     public boolean subscription;
-    public FormDate birthday;
 
 
 }
